151cef80fccf2824ee053d607b4a72f8a504ca9e
Manuel Astudillo <manuel@optimalbits.com>
2012-10-03 09:23:59 +0200

    Added a few unit tests for gzip compression.

---
+++ test/test-compression.js
@@ -0,0 +1,38 @@
+
+var cabinet = require('..'),
+    send = require('send'),
+    http = require('http'),
+    connect = require('connect'),
+    request = require('supertest'),
+    expect = require('expect.js');
+
+var app = connect();
+
+app.use(cabinet(__dirname + '/fixtures', {
+  gzip:true,
+  cache:{
+    maxSize: 1024, 
+    maxObjects:256
+  },
+  coffee:true,
+  stylus:{
+    paths:[]
+  }
+}));
+
+describe('File Cabinet With Cache and Gzip Enabled', function(){
+  it('should serve unzipped files', function(done){
+    request(app)
+    .get('/todo.txt')
+    .expect('Content-Type', 'text/plain; charset=UTF-8')
+    .expect('- groceries', done);
+  });
+  
+  it('should serve a coffe script as a zipped file', function(done){
+    request(app)
+      .get('/array.coffee')
+      .set('accept-encoding', 'gzip')
+      .expect('Content-Encoding', 'gzip')
+      .expect('Content-Length', '721', done)
+  });  
+});
